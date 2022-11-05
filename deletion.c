7eb48bf06b0104eafa5894a6871082cec5339999
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-05 19:25:29 +0530

    Update deletion.c

--- deletion.c
+++ deletion.c
@@ -2,12 +2,15 @@
 
 void display(int arr[],int n)
 {
+    
     for(int i=0;i<=n;i++)
     {
         printf("%d ",arr[i]);
     }
     printf("\n\n");
+    
 }
+
 
 void deletion(int arr[],int size,int index,int capacity)
 {
