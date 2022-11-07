3ec61e4f31e7e64559ab6d6ef9108f43521d22f0
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-07 21:47:52 +0530

    Add files via upload

---
+++ linkedlist_insertion.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+void show(struct Node *ptr)
+{
+    while (ptr!=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+    
+
+}
+
+struct Node * insertionatfirst(struct Node *head,int data)
+{
+    struct Node *ptr=(struct Node *)malloc(sizeof(struct Node));
+    ptr->next=head;
+    ptr->data=data;
+    return ptr;
+}
+
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third; 
+
+    //Allocaste memory for nodes in the linked list in heap
+    head=(struct Node *) malloc(sizeof(struct Node));
+    second=(struct Node *) malloc(sizeof(struct Node));
+    third=(struct Node *) malloc(sizeof(struct Node));
+    head->data=7;
+    head->next=second;
+
+    second->data=8;
+    second->next=third;
+
+    third->data=9;
+    third->next=NULL;
+    show(head);  //head cause we are starting from head
+    printf("after insertion\n");
+    head=insertionatfirst(head,85);
+    show(head);
+    return 0;
+
+}
