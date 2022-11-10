b576abd92b30baab40dee3ea45e14c84434c8a4b
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-10 20:33:09 +0530

    Update linkedlist_insertion.c

--- linkedlist_insertion.c
+++ linkedlist_insertion.c
@@ -28,14 +28,59 @@ struct Node * insertionatfirst(struct Node *head,int data)
 
 int main()
 {
-    struct Node *head;
-    struct Node *second;
-    struct Node *third; 
+    #include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+void show(struct Node *ptr)
+{
+    while (ptr!=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+    
+
+}
+
+struct Node * insertionatfirst(struct Node *head,int data)
+{
+    struct Node *ptr=(struct Node *)malloc(sizeof(struct Node));
+    ptr->next=head;
+    ptr->data=data;
+    return ptr;
+}
+
+
+int main()
+{
+    struct Node *head=(struct Node *) malloc(sizeof(struct Node));
+    struct Node *second=(struct Node *) malloc(sizeof(struct Node));
+    struct Node *third=(struct Node *) malloc(sizeof(struct Node));
 
     //Allocaste memory for nodes in the linked list in heap
-    head=(struct Node *) malloc(sizeof(struct Node));
-    second=(struct Node *) malloc(sizeof(struct Node));
-    third=(struct Node *) malloc(sizeof(struct Node));
+    //head=(struct Node *) malloc(sizeof(struct Node));
+    // second=(struct Node *) malloc(sizeof(struct Node));
+    // third=(struct Node *) malloc(sizeof(struct Node));
+    head->data=7;
+    head->next=second;
+
+    second->data=8;
+    second->next=third;
+
+    third->data=9;
+    third->next=NULL;
+    show(head);  //head cause we are starting from head
+    printf("after insertion\n");
+    head=insertionatfirst(head,85);
+    show(head);
+    return 0;
+
+}
     head->data=7;
     head->next=second;
 
